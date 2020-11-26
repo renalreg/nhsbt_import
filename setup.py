836eccb198b83d189735c03dfe4352d3e2d4d399
George Swinnerton <george.swinnerton@renalregistry.nhs.uk>
2020-11-26 09:45:22 +0000

    Handle more complex requirements.txt lines

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
+def parse_req_line(line: str): -> str:
+    package = line.split(";")[0]
+    package = package.strip()
+    
+    return package
+
 with open("requirements.txt") as f:
-    install_req = [x.strip() for x in f.readlines() if not x.startswith("-e")]
+    install_req = [parse_req_line(x) for x in f.readlines() if not x.startswith("-e")]
 
 setup(
     name='rr_ukt_import',
